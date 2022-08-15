208e28c050f9d62abefee0bb49e6ac3098d3a264
Shulav <soluvp@gmail.com>
2022-08-15 10:52:43 +0545

    Little things done

--- config/config.py
+++ config/config.py
@@ -1,7 +1,16 @@
-Ingredients = []
 api_key="3b7173107d9643a5bd00c244ea1817de"
 
-
 def add_ingredient(ingre):
-    Ingredients.append(ingre)
+    with open('.\config\Ingredients.txt', 'r+') as f:
+        first = f.read()
+        if not first:
+            f.write(ingre)
+        else:
+            f.write("," + ingre)
+        print(first)
+        listRes = list(first.split(","))
+        print(listRes)
+
+
+
 
--- main.py
+++ main.py
@@ -4,32 +4,47 @@ import csv
 from config import config
 import pandas as pd
 apikey=config.api_key
+try:
+    def getRecipeByIngredients(Ingredients):
+        payload = {
+            'fillIngredients': False,
+            'ingredients': Ingredients,
+            'limitLicense': False,
+            'number': 2,
+            'ranking': 1
+        }
+        endpoint="https://api.spoonacular.com/recipes/findByIngredients"
+        headers = {
+            'x-api-key': apikey
+                    }
+        observation= requests.get(endpoint, params=payload, headers=headers)
+        observation = observation.json()
+        print(observation)
+        config.add_ingredient(Ingredients)
+        return observation
+except:
+    print("Provide ingredient is not in our any receipe")
+input1=input("search the receipe by single ingredient:")
+inputforjson=getRecipeByIngredients(input1)
 
-# searching the receipe by ingredient
-Ingredient=input("Enter the ingredient to search the receipe")
-config.add_ingredient(Ingredient)
-print(config.Ingredients)
-
-url="https://api.spoonacular.com/recipes/findByIngredients?ingredients="+Ingredient+ "&number=1&apiKey=" + apikey
-observation= requests.get(url)
-print(observation.json())
-
-# aheley testing ko lagi matra function nabhanako
-# def json_creator():
-data = observation.json()
+# # aheley testing ko lagi matra function nabhanako
+# # def json_creator():
+data = inputforjson
 s=json.dumps(data)
 with open(".\jsonfiles\data1.json",'w') as f:
     f.write(s)
-
-# converting json to csv
+    #
+# # converting json to csv
 df = pd.read_json (r'.\jsonfiles\data1.json')
 df.to_csv (r'.\csvfiles\data1.csv', index = None)
-
-# df.to_csv (r'Path where the new CSV file will be stored\New File Name.csv', index = None)
-
+#
+# # df.to_csv (r'Path where the new CSV file will be stored\New File Name.csv', index = None)
 dc = pd.read_csv (r'.\csvfiles\data1.csv')
+
+#
 with open(".\csvfiles\data2.csv",'a',newline="") as f:
     writer = csv.writer(f)
     writer.writerow(dc)
+
 
 
